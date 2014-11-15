e835f74d2cc05b37f4ba4eb8e7262dc973d2b450
Adam Nuttall <adam@adamnuttall.co.uk>
2014-11-15 18:25:52 +0000

    Initial commit

---
+++ Houses/App_Start/HousesContext.cs
@@ -0,0 +1,21 @@
+﻿using Houses.Properties;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Houses.App_Start
+{
+    public class HousesContext
+    {
+        public MongoDatabase Database;
+
+        public HousesContext()
+        {
+            var client = new MongoClient(Settings.Default.HousesConnectionString);
+            var server = client.GetServer();
+            Database = server.GetDatabase(Settings.Default.HousesDatabaseName);
+        }
+    }
+}
---
+++ Houses/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Houses.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Houses.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HousesContext Context = new HousesContext();
+
+        public ActionResult Index()
+        {
+            Context.Database.GetStats();
+            return Json(Context.Database.Server.BuildInfo, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
