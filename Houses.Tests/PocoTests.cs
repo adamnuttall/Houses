5e0435cfd66b7b20b96c25cae15294952c7454bd
Adam Nuttall <adam@adamnuttall.co.uk>
2014-11-15 19:47:54 +0000

    Base Rental class with tests

--- Houses.Tests/PocoTests.cs
+++ Houses.Tests/PocoTests.cs
@@ -12,6 +12,22 @@ namespace Houses.Tests
             JsonWriterSettings.Defaults.Indent = true;
         }
 
-        
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public int Age { get; set; }
+        }
+
+        [TestMethod]
+        public void Automatic()
+        {
+            var person = new Person
+            {
+                Age = 29,
+                FirstName = "Adam"
+            };
+
+            Console.WriteLine(person.ToString());
+        }
     }
 }
---
+++ Houses/Rentals/Rental.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Houses.Rentals
+{
+    public class Rental
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public int NumberOfRooms { get; set; }
+        public List<string> Address = new List<string>();
+        [BsonRepresentation(BsonType.Double)]
+        public decimal Price { get; set; }
+    }
+}
